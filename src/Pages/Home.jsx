0a3e8b97ca0549f704258014edf21a08c98c3476
far-han12 <mahmudfarhan590@gmail.com>
2024-04-26 03:44:30 +0600

    footer and errorpage  Created

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from "../Components/NavBar"
 import Banner from '../Components/Banner';
+import Footer from '../Components/Footer';
 
 const Home = () => {
     return (
         <div>
             <div className='mt-3 mb-10'><NavBar></NavBar></div>
             <div className='mb-10'><Banner></Banner></div>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,11 +4,13 @@ import Home from "../Pages/Home";
 import {
     createBrowserRouter,
   } from "react-router-dom";
+import ErrorPage from '../Pages/ErrorPage';
 
   const routes = createBrowserRouter([
     {
       path: "/",
       element:<MainLayout></MainLayout> ,
+      errorElement:<ErrorPage></ErrorPage>,
       children:[
         {
           path:"/",
