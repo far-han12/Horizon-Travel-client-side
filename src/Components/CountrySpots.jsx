56e61228cdcb136af7edb25760d48239a7c25410
far-han12 <mahmudfarhan590@gmail.com>
2024-08-14 14:30:43 +0600

    edited country

--- src/Components/CountrySpots.jsx
+++ src/Components/CountrySpots.jsx
@@ -1,22 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import TouristCard from './TouristCard';
-
+import NavBar from './NavBar';
 const CountrySpots = () => {
   const { country_Name } = useParams();
-  const [spots, setSpots] = useState([]);
+  const [spot, setspot] = useState([]);
   useEffect(() => {
     fetch(`https://react-tourism-server.vercel.app/spot/bycountry/${country_Name}`)
       .then(res => res.json())
-      .then(data=> setSpots(data))
+      .then(data=> setspot(data))
   },[country_Name])
   return (
     <div>
-      <h1>hi</h1>
-      {spots.map((s, p) => (
-                  <div key={p} className="  p-4  transform transition duration-500 hover:scale-105">
-                    <TouristCard s={s} />
-                  </div>
+      <NavBar></NavBar>
+     <h1 className='font-bold text-center my-4 text-xl md:text-3xl lg:text-4xl'>Tourists Spots of {country_Name}</h1>
+      
+     {spot.map((s, p) => (
+                    <TouristCard s={s} key={p} />
+                 
                 ))}
     </div>
   );
